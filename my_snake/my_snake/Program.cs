1ef3b25f810052276e5de3f0b3ee276808dabf52
pgpryadkin <pgpryadkin@gmail.com>
2018-01-12 23:36:32 +0500

    create class Point (reverted from commit b60372186557915faa20495d21442a4617ad1228)

--- my_snake/my_snake/Program.cs
+++ my_snake/my_snake/Program.cs
@@ -10,12 +10,27 @@ namespace my_snake
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 2;
+            p1.sym = '*';
+
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+
+            p2.Draw();
+
+            /*
             int x1 = 1;
             int y1 = 2;
             char sym1 = '*';
 
-            Draw(x1, y1, sym1);
-        
+            Draw(x1, y1, sym1);*/
+
             Console.ReadLine();
         }
         static void Draw(int x, int y, char sym)
