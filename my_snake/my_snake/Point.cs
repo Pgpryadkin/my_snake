6d740233acfc6e23b80572108233144a89e9356c
pgpryadkin <pgpryadkin@gmail.com>
2018-01-14 11:18:16 +0500

    reload project

--- my_snake/my_snake/Point.cs
+++ my_snake/my_snake/Point.cs
@@ -4,18 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace my_snake
+namespace Snake
 {
-    class Point
-    {
-        public int x;
-        public int y;
-        public char sym;
+	class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
 
-        public void Draw()
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
-    }
+		public Point()
+		{
+		}
+
+		public Point(int _x, int _y, char _sym)
+		{
+			x = _x;
+			y = _y;
+			sym = _sym;
+		}
+
+		public void Draw()
+		{
+			Console.SetCursorPosition( x, y );
+			Console.Write( sym );			
+		}
+	}
 }
--- my_snake/my_snake/Program.cs
+++ my_snake/my_snake/Program.cs
@@ -4,27 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace my_snake
+namespace Snake
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 2;
-            p1.sym = '*';
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Point p1 = new Point(1, 3, '*');
+			p1.Draw();
 
-            p1.Draw();
+			Point p2 = new Point(4, 5, '#');
+			p2.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
-
-            p2.Draw();
-
-            Console.ReadLine();
-        }
-    }
+			Console.ReadLine();
+		}
+	}
 }
